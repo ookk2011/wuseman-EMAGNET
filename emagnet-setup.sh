77a72b6072e6a93b3220fe28bf7c53e5f1eda58b
wuseman <info@wuseman.com>
2019-03-11 17:07:15 +0100

    edited all lines after all commands was added

--- emagnet-setup.sh
+++ emagnet-setup.sh
@@ -332,20 +332,20 @@ if [[ -f $LYNX && -f $ELINKS ]]; then
   printf  "+====================================================================+\n"
   printf "It seems you have both lynx and elinks2 installed, you must choose one\n"; read -p "Option: (lynx/elinks): " browsertouse
 if [[ $browsertouse = "lynx" ]]; then
-  sed -i "224d" $SCRIPT
-  sed -i '224i lynx -dump \$PASTEBIN | sed "s/com\\\//com\\\/raw\\\//g" | grep -o http.* | sed -n "7,14p" > /tmp/.emagnet' $SCRIPT
+  sed -i "315d" $SCRIPT
+  sed -i '315i lynx -dump \$PASTEBIN | sed "s/com\\\//com\\\/raw\\\//g" | grep -o http.* | sed -n "7,14p" > /tmp/.emagnet' $SCRIPT
   printf "\nConfig file has been updated, lynx will be used for downloading files\n\n"
 else
-  sed -i "224d" $SCRIPT
-  sed -i "224i elinks -dump \$PASTEBIN| sed 's/com\\\//com\\\/raw\\\//g' | grep -o http.* | sed -n '13,20p' > \/tmp\/.emagnet" $SCRIPT
+  sed -i "315d" $SCRIPT
+  sed -i "315i elinks -dump \$PASTEBIN| sed 's/com\\\//com\\\/raw\\\//g' | grep -o http.* | sed -n '13,20p' > \/tmp\/.emagnet" $SCRIPT
   printf "\nConfig file has been updated, elinks will be used for downloading files\n\n"
 fi
 elif [[ -f $LYNX && ! -f $ELINKS ]]; then
-  sed -i "224d" $SCRIPT
-  sed -i '224i lynx -dump \$PASTEBIN | sed "s/com\\\//com\\\/raw\\\//g" | grep -o http.* | sed -n "7,14p" > /tmp/.emagnet' $SCRIPT
+  sed -i "315d" $SCRIPT
+  sed -i '315i lynx -dump \$PASTEBIN | sed "s/com\\\//com\\\/raw\\\//g" | grep -o http.* | sed -n "7,14p" > /tmp/.emagnet' $SCRIPT
 elif [[ ! -f $LYNX && -f $ELINKS ]]; then
-  sed -i "224d" $SCRIPT
-  sed -i "224i elinks -dump \$PASTEBIN| sed 's/com\\\//com\\\/raw\\\//g' | grep -o http.* | sed -n '13,20p' > \/tmp\/.emagnet" $SCRIPT
+  sed -i "315d" $SCRIPT
+  sed -i "315i elinks -dump \$PASTEBIN| sed 's/com\\\//com\\\/raw\\\//g' | grep -o http.* | sed -n '13,20p' > \/tmp\/.emagnet" $SCRIPT
 else
   printf  "+====================================================================+\n"
   printf  "#                                                                    #\n"
@@ -369,8 +369,8 @@ else
       if [[ $DISTRO = "mint" ]]; then apt-get install elinks;
        requirements;idletime;idletime;wip;emagnethome;wgettimer;settime;exit 0;fi
       if [[ -n $DISTRO ]]; then echo "Emagnet is not supported for $DISTRO, please install elinks manually."; exit 0; fi 
-      sed -i "224d" $SCRIPT
-      sed -i "224i elinks -dump \$PASTEBIN| sed 's/com\\\//com\\\/raw\\\//g' | grep -o http.* | sed -n '13,20p' > \/tmp\/.emagnet" $SCRIPT
+      sed -i "315d" $SCRIPT
+      sed -i "315i elinks -dump \$PASTEBIN| sed 's/com\\\//com\\\/raw\\\//g' | grep -o http.* | sed -n '13,20p' > \/tmp\/.emagnet" $SCRIPT
        printf "\nConfig file has been updated, elinks will be used when downloading files from pastebin" $SCRIPT ;;
    lynx)
       printf "\nGoing to install $browsertouse2, setup will continue when $browsertouse2 has been installed..\n\n"
@@ -385,8 +385,8 @@ else
       if [[ $DISTRO = "mint" ]]; then apt-get install lynx;
        requirements;idletime;idletime;wip;emagnethome;wgettimer;settime;exit 0;fi
       if [[ -n $DISTRO ]]; then echo "Emagnet is not supported for $DISTRO, please install lynx or elinks manually."; exit 0; fi
-       sed -i "224d" $SCRIPT
-       sed -i '224i lynx -dump \$PASTEBIN | sed "s/com\\\//com\\\/raw\\\//g" | grep -o http.* | sed -n "7,14p" > /tmp/.emagnet' $SCRIPT
+       sed -i "315d" $SCRIPT
+       sed -i '315i lynx -dump \$PASTEBIN | sed "s/com\\\//com\\\/raw\\\//g" | grep -o http.* | sed -n "7,14p" > /tmp/.emagnet' $SCRIPT
        printf "\nConfig file has been updated, lynx will be used when downloading files from pastebin" $SCRIPT ;;
    N) exit 0 ;;
    \?) echo "Please enter a proper answer y=yes N=no" ;;
