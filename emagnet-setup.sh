05ccb4aa89218f20b7bfaa29b99af97a81dd0b45
wuseman <wuseman@nr1.nu>
2019-04-10 07:18:19 +0000

    another time

--- emagnet-setup.sh
+++ emagnet-setup.sh
@@ -341,7 +341,7 @@ know which one you prefer it doesn't matter, just choose one.
     printf "It seems you have both lynx and elinks2 installed, you must choose one\n"; read -p "Option: (lynx/elinks): " browsertouse
 if [[ $browsertouse = "lynx" ]]; then
     sed -i "490d" $SCRIPT;sed -i "490i lynx -dump \$PASTEBIN|grep https|cut -c 7-|sed 's/com/com\\\/raw/g'|awk 'length(\$0)>32 && length(\$0)<35\'|grep -v 'messages\\\|settings\\\|languages\\\|archive\\\|facebook\\\|scraping' > /tmp/.emagnet" $SCRIPT
-    sed -i "795d" $SCRIPT;sed -i '795i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT;sed -i '682i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT
+    sed -i "795d" $SCRIPT;sed -i '795i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT;sed -i "578d" $SCRIPT;sed -i '578i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT
     printf "\nConfig file has been updated, lynx will be used for downloading files\n";else
     sed -i "490d" $SCRIPT;sed -i "490i elinks -dump \$PASTEBIN|grep https|cut -c 7-|sed 's/com/com\\\/raw/g'|awk 'length(\$0)>32 && length(\$0)<35\'|grep -v 'messages\\\|settings\\\|languages\\\|archive\\\|facebook\\\|scraping' > /tmp/.emagnet" $SCRIPT
     sed -i "795d" $SCRIPT;sed -i '795i elinks -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT
@@ -349,7 +349,7 @@ if [[ $browsertouse = "lynx" ]]; then
     sed -i "490d" $SCRIPT;sed -i "490i lynx -dump \$PASTEBIN|grep https|cut -c 7-|sed 's/com/com\\\/raw/g'|awk 'length(\$0)>32 && length(\$0)<35\'|grep -v 'messages\\\|settings\\\|languages\\\|archive\\\|facebook\\\|scraping' > /tmp/.emagnet " $SCRIPT
     sed -i "795d" $SCRIPT;sed -i '795i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT; elif [[ ! -f $LYNX && -f $ELINKS ]]; then
     sed -i "490d" $SCRIPT;sed -i "490i elinks -dump \$PASTEBIN|grep https|cut -c 7-|sed 's/com/com\\\/raw/g'|awk 'length(\$0)>32 && length(\$0)<35\'|grep -v 'messages\\\|settings\\\|languages\\\|archive\\\|facebook\\\|scraping' > /tmp/.emagnet" $SCRIPT
-      sed -i "795d" $SCRIPT;sed -i '795i elinks -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT;sed -i "682d" $SCRIPT;sed -i '682i elinks -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT;else
+      sed -i "795d" $SCRIPT;sed -i '795i elinks -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT;sed -i "578d" $SCRIPT;sed -i '578i elinks -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT;else
     echo -e "\n\n\033[1mBROWSER SETUP:\033[0m\n\033[1m----------------------\033[0m
 Choose wich browser you prefer to use when emagnet will visit
 pastebin.com for the latest uploads urls on site. If you don't
@@ -388,8 +388,12 @@ read -p "Prefered browser to install: (lynx/elinks): " browsertouse2
        requirements;idletime;idletime;wip;emagnethome;wgettimer;settime;exit 0;fi
       if [[ -n $DISTRO ]]; then echo "Emagnet is not supported for $DISTRO, please install lynx or elinks manually."; exit 0; fi
        sed -i "490d" $SCRIPT
-       sed -i "490d" $SCRIPT;sed -i "490i lynx -dump \$PASTEBIN|grep https|cut -c 7-|sed 's/com/com\\\/raw/g'|awk 'length(\$0)>32 && length(\$0)<35\'|grep -v 'messages\\\|settings\\\|languages\\\|archive\\\|facebook\\\|scraping' > /tmp/.emagnet" $SCRIPT
-       sed -i "490d" $SCRIPT;sed -i '795i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT; sed -i "682d" $SCRIPT;sed -i '682i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT;
+       sed -i "490d" $SCRIPT
+       sed -i "490i lynx -dump \$PASTEBIN|grep https|cut -c 7-|sed 's/com/com\\\/raw/g'|awk 'length(\$0)>32 && length(\$0)<35\'|grep -v 'messages\\\|settings\\\|languages\\\|archive\\\|facebook\\\|scraping' > /tmp/.emagnet" $SCRIPT
+       sed -i "490d" $SCRIPT;
+       sed -i '795i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT
+       sed -i "578d" $SCRIPT
+       sed -i '578i lynx -dump $PASTEBIN > /tmp/.emagnet' $SCRIPT
        printf "\nConfig file has been updated, lynx will be used when downloading files from pastebin" $SCRIPT ;;
    N) exit 0 ;;
    \?) echo "Please enter a proper answer y=yes N=no" ;;
