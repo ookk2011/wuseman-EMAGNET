e893bb3ddece1d84b8e6b17ff638f427021ed61c
wuseman <wuseman@nr1.nu>
2019-07-19 07:12:51 +0200

    edited browser line

--- emagnet-setup.sh
+++ emagnet-setup.sh
@@ -339,16 +339,16 @@ know which one you prefer it doesn't matter, just choose one.
 "
     printf "It seems you have both lynx and elinks2 installed, you must choose one\n"; read -p "Option: (lynx/elinks): " browsertouse
 if [[ $browsertouse = "lynx" ]]; then
-    sed -i "181d" $CONF;sed -i '181 i BROWSER=lynx' $CONF
+    sed -i "179d" $CONF;sed -i '179 i BROWSER=lynx' $CONF
     printf "\nConfig file has been updated, lynx will be used for downloading files\n"
     else
-    sed -i "181d" $CONF;sed -i '181 i BROWSER=elinks' $CONF
+    sed -i "179d" $CONF;sed -i '179 i BROWSER=elinks' $CONF
     printf "\nConfig file has been updated, elinks will be used for downloading files\n"
     fi
     elif [[ -f $LYNX && ! -f $ELINKS ]]; then
-    sed -i "181d" $CONF;sed -i '181 i BROWSER=lynx' $CONF
+    sed -i "179d" $CONF;sed -i '179 i BROWSER=lynx' $CONF
     elif [[ ! -f $LYNX && -f $ELINKS ]]; then
-    sed -i "181d" $CONF;sed -i '181 i BROWSER=elinks' $CONF
+    sed -i "179d" $CONF;sed -i '179 i BROWSER=elinks' $CONF
     else
     echo -e "\n\n\033[1mBROWSER SETUP:\033[0m\n\033[1m----------------------\033[0m
 Choose wich browser you prefer to use when emagnet will visit
@@ -371,7 +371,7 @@ read -p "Prefered browser to install: (lynx/elinks): " browsertouse2
       if [[ $DISTRO = "mint" ]]; then apt-get install elinks;
        requirements;wip;emagnethome;wgettimer;settime;exit 0;fi
       if [[ -n $DISTRO ]]; then echo "Emagnet is not supported for $DISTRO, please install elinks manually."; exit 0; fi 
-       sed -i "181d" $CONF;sed -i '181 i BROWSER=elinks' $CONF
+       sed -i "179d" $CONF;sed -i '179 i BROWSER=elinks' $CONF
        printf "\nConfig file has been updated, elinks will be used when downloading files from pastebin" $SCRIPT ;;
    lynx)
       printf "\nGoing to install $browsertouse2, setup will continue when $browsertouse2 has been installed..\n\n"
@@ -386,7 +386,7 @@ read -p "Prefered browser to install: (lynx/elinks): " browsertouse2
       if [[ $DISTRO = "mint" ]]; then apt-get install lynx;
        requirements;wip;emagnethome;wgettimer;settime;exit 0;fi
       if [[ -n $DISTRO ]]; then echo "Emagnet is not supported for $DISTRO, please install lynx or elinks manually."; exit 0; fi
-       sed -i "181d" $CONF;sed -i '181 i BROWSER=lynx' $CONF
+       sed -i "179d" $CONF;sed -i '179 i BROWSER=lynx' $CONF
        printf "\nConfig file has been updated, lynx will be used when downloading files from pastebin" $SCRIPT ;;
    N) exit 0 ;;
    \?) echo "Please enter a proper answer y=yes N=no" ;;
